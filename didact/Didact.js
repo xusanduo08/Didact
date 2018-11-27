85efe00774099bb2e666444e8c27efff3716d822
xusanduo08 <2279190887@qq.com>
2018-11-27 21:59:31 +0800

    更新代码

--- didact/Didact.js
+++ didact/Didact.js
@@ -1,40 +1,104 @@
 const TEXT_ELEMENT = 'TEXT_ELEMENT';
 
-function createElement(type, config, ...args){
+function createElement(type, config, ...args) {
   const props = Object.assign({}, config);
   const hasChildren = args.length > 0;
   const rawChildren = hasChildren ? [].concat(...args) : [];
 
   props.children = rawChildren.filter(c => c != null && c !== false)
     .map(c => c instanceof Object ? c : createTextElement(c));
-  
-  return { type, props};
+
+  return { type, props };
 }
 
 function createTextElement(value) {
-  return createElement(TEXT_ELEMENT, { nodeValue: value});
+  return createElement(TEXT_ELEMENT, { nodeValue: value });
 }
 
-function render(element, parentDom){
+let rootInstance = null;
+
+function render(element, container) {
+  const prevProps = rootInstance;
+  const nextInstance = reconcile(container, prevProps, element);
+  rootInstance = nextInstance;
+}
+
+// 实例化一个元素（就是将元素对应的DOM放到对应父DOM中，只有新增的元素需要实例化）
+function instantiate(element) {
   const { type, props } = element;
   const isTextElement = type === 'TEXT_ELEMENT';
   const dom = isTextElement ? document.createTextNode('') : document.createElement(type);
-
-  const isListener = name => name.startsWith('on');
-  Object.keys(props).filter(isListener).forEach(name => {
-    const eventType = name.toLocaleLowerCase().substring(2);
-    dom.addEventListener(eventType, props[name]);
-  })
-
-  const isAttribute = name => !isListener(name) && name != 'children';
-  Object.keys(props).filter(isAttribute).forEach(name => {
-    dom[name] = props[name]; // 文本节点的文本值通过nodeValue属性渲染到页面上
-  })
+  updateDomProperties(dom, [], props);
 
   const childElements = props.children || [];
-  childElements.forEach(childElement => render(childElement, dom));
-  
-  parentDom.appendChild(dom);
+  const childInstances = childElements.map(instantiate); // 递归实例化子元素
+  const childDoms = childInstances.map(childInstance => childInstance.dom);
+  childDoms.forEach(childDom => dom.appendChild(childDom));
+
+  const instance = { dom, element, childInstances };
+  return instance;
+}
+
+// 更新DOM节点属性
+function updateDomProperties(dom, prevProps, nextProps){
+  const isEvent = name => name.startsWith("on");
+  const isAttribute = name => !isEvent(name) && name != "children";
+
+  Object.keys(prevProps).filter(isEvent).forEach(name => {
+    const eventType = name.toLowerCase().substring(2);
+    dom.removeEventListener(eventType, prevProps[name]);
+  });
+
+  Object.keys(prevProps).filter(isAttribute).forEach(name => {
+    dom[name] = null;
+  });
+
+  Object.keys(nextProps).filter(isAttribute).forEach(name => {
+    dom[name] = nextProps[name];
+  });
+
+  Object.keys(nextProps).filter(isEvent).forEach(name => {
+    const eventType = name.toLowerCase().substring(2);
+    dom.addEventListener(eventType, nextProps[name]);
+  });
+}
+
+// 一致性校验
+function reconcile(parentDom, instance, element){
+  if(instance == null){ // 有新增的元素
+    const newInstance = instantiate(element);
+    parentDom.appendChild(newInstance.dom);
+    return newInstance
+  } else if(element == null){ // 需要删除一些节点
+    parentDom.removeChild(instance.dom);
+    return null;
+  } else if(instance.element.type === element.type){ // 类型相同，更新节点
+    updateDomProperties(instance.dom, instance.element.props, element.props); // 更新属性
+    instance.childInstances = reconcileChildren(instance, element); // 对子元素进行一致性校验
+    instance.element = element;
+    return instance; 
+  } else {
+    const newInstance = instantiate(element);
+    parentDom.replaceChild(newInstance.dom, instance.dom);
+    return newInstance;
+  }
+}
+
+function reconcileChildren(instance, element){
+  const dom = instance.dom;
+  const childInstances = instance.childInstances;
+  const nextChildElements = element.props.children || [];
+  const newChildInstances = [];
+  const count = Math.max(childInstances.length, nextChildElements.length);
+
+  for(let i = 0; i < count; i++){
+    const childInstance = childInstances[i];
+    const childElement = nextChildElements[i];
+    const newChildInstance = reconcile(dom, childInstance, childElement);
+    newChildInstances.push(newChildInstance);
+  }
+
+  return newChildInstances.filter(instance => instance != null);
 }
 
 export default {
--- didact/index.js
+++ didact/index.js
@@ -1,24 +1,50 @@
 /** @jsx Didact.createElement */
 import Didact from './Didact';
 
+const randomLikes = () => Math.ceil(Math.random() * 100);
+
 const stories = [
-  { name: "Didact introduction", url: "http://bit.ly/2pX7HNn" },
-  { name: "Rendering DOM elements ", url: "http://bit.ly/2qCOejH" },
-  { name: "Element creation and JSX", url: "http://bit.ly/2qGbw8S" },
-  { name: "Instances and reconciliation", url: "http://bit.ly/2q4A746" },
-  { name: "Components and state", url: "http://bit.ly/2rE16nh" }
+  {
+    name: "Didact introduction",
+    url: "http://bit.ly/2pX7HNn",
+    likes: randomLikes()
+  },
+  {
+    name: "Rendering DOM elements ",
+    url: "http://bit.ly/2qCOejH",
+    likes: randomLikes()
+  },
+  {
+    name: "Element creation and JSX",
+    url: "http://bit.ly/2qGbw8S",
+    likes: randomLikes()
+  },
+  {
+    name: "Instances and reconciliation",
+    url: "http://bit.ly/2q4A746",
+    likes: randomLikes()
+  },
+  {
+    name: "Components and state",
+    url: "http://bit.ly/2rE16nh",
+    likes: randomLikes()
+  }
 ];
 
-const appElement = <div><ul>{stories.map(storyElement)}</ul></div>;
+const appElement = () => <div><ul>{stories.map(storyElement)}</ul></div>;
 
-function storyElement({ name, url }) {
-  const likes = Math.ceil(Math.random() * 100);
+function storyElement(story) {
   return (
     <li>
-      <button>{likes}❤️</button>
-      <a href={url}>{name}</a>
+      <button onClick={e => handleClick(story)}>{story.likes}<b>❤️</b></button>
+      <a href={story.url}>{story.name}</a>
     </li>
   );
 }
 
-Didact.render(appElement, document.getElementById("root"));
+function handleClick(story) {
+  story.likes += 1;
+  Didact.render(appElement(), document.getElementById("root"));
+}
+
+Didact.render(appElement(), document.getElementById("root"));
