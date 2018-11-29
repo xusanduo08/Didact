8abfb784e8ff3d58d6e2201212bb3a765d540e01
xusanduo08 <2279190887@qq.com>
2018-11-29 22:12:19 +0800

    添加注释

--- didact/Didact.js
+++ didact/Didact.js
@@ -1,5 +1,6 @@
 const TEXT_ELEMENT = 'TEXT_ELEMENT';
 
+// 创建Didact元素，返回含有type、props属性的对象
 function createElement(type, config, ...args) {
   const props = Object.assign({}, config);
   const hasChildren = args.length > 0;
