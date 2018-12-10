dcc16f09fcbf65058b2cebc4717c1f4a9596b089
xusanduo08 <2279190887@qq.com>
2018-12-10 22:02:21 +0800

    reconcile:render、scheduleUpdate、performWork

--- didact/reconciler.js
+++ didact/reconciler.js
@@ -1,2 +1,48 @@
 import { createDomElement, updateDomProperties } from './dom-utils';
 import { createInstance } from './component';
+
+
+// Fiber tags
+const HOST_COMPONENT = 'host';
+const CLASS_COMPONENT = 'class';
+const HOST_ROOT = 'root';
+
+// Effect tags
+const PLACEMENT = 1;
+const DELETION = 2;
+const UPDATE = 3;
+
+const ENOUGH_TIME = 1;
+
+// Global state
+const updateQueue = [];
+let nextUnitOfWork = null;
+let pendingCommit = null;
+
+export function render(elements, containerDom) {
+  updateQueue.push({
+    from: HOST_ROOT,
+    dom: containerDom,
+    newProps: { children: elements }
+  });
+
+  requestIdleCallback(performWork)
+}
+
+// 组件的setState()中会调用该方法。
+export function scheduleUpdate(instance, partialState){
+  updateQueue.push({
+    from: CLASS_COMPONENT,
+    instance: instance,
+    partialState: partialState
+  });
+  requestIdleCallback(performWork);
+}
+
+function performWork(deadline){
+  workLoop(deadline);
+  if(nextUnitOfWork || updateQueue.length > 0){
+    requestIdleCallback(performWork);
+  }
+}
+
